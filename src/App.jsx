34a19543fc54376ff54df2c01bc010c854d81ec4
Riktam Nandi <riktamrdxnandi@gmail.com>
2025-02-25 20:45:29 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Weather from "./components/Weather";
+import background from "./assets/background.svg";
 
 function App() {
   return (
-    <>
+    <div className="bg-blue-300 min-h-screen">
       <Weather />
-    </>
+    </div>
   );
 }
 
